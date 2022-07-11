1a3ac1789231067c4eaccc4326f9bf707d977bcf
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-07-11 23:53:52 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+const speedBtn = document.querySelector('#speed');
+
+
+function handleStartGame(){
+    console.log('start!');
+}
+startBtn.addEventListener('click', handleStartGame)
